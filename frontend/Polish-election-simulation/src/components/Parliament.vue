eb473455cd62f1ac8ce62f9c9ca833d3de718993
Piotr Janiszewski <piotr.3.janiszewski@gmail.com>
2025-11-23 14:59:45 +0100

    Refactored Parliament.vue

--- frontend/Polish-election-simulation/src/components/Parliament.vue
+++ frontend/Polish-election-simulation/src/components/Parliament.vue
@@ -1,15 +1,20 @@
 <template>
-  <div>
-    <svg :width="2 * outerRadius" :height="outerRadius" xmlns="http://www.w3.org/2000/svg">
+  <div class="parliament-container">
+    <svg 
+      :width="2 * outerRadius" 
+      :height="outerRadius" 
+      xmlns="http://www.w3.org/2000/svg"
+    >
       <circle
         v-for="(dot, index) in dots"
         :key="index"
-        :cx="outerRadius + orbits[dot.position.orbit].radius * Math.cos(orbits[dot.position.orbit].anglePerBall * (dot.position.index + 0.5))"
-        :cy="outerRadius - orbits[dot.position.orbit].radius * Math.sin(orbits[dot.position.orbit].anglePerBall * (dot.position.index + 0.5))"
+        :cx="calculateDotX(dot)"
+        :cy="calculateDotY(dot)"
         :r="dotRadius * 0.7"
         :fill="getColorForDot(dot.colorIndex)"
       />
     </svg>
+    
     <div class="legend-container">
       <div 
         v-for="(party, index) in resultsToDisplay"
@@ -17,176 +22,229 @@
         class="legend-item"
       >
         <svg :width="dotRadius * 2" :height="dotRadius * 2">
-          <circle :cx="dotRadius" :cy="dotRadius" :r="dotRadius * 0.8" :fill="getColorForDot(index)" />
+          <circle 
+            :cx="dotRadius" 
+            :cy="dotRadius" 
+            :r="dotRadius * 0.8" 
+            :fill="getColorForDot(index)" 
+          />
         </svg>
         <span class="legend-text">{{ party.partyName }} ({{ party.seats }})</span>
       </div>
-    </div> 
+    </div>
   </div>
 </template>
 
-<script setup lang='ts'>
-  import ResultsTableRow from '../api/ResultsTableRow.ts'
+<script setup lang="ts">
+import { computed } from 'vue'
+import type ResultsTableRow from '../api/ResultsTableRow.ts'
 
-  const props = defineProps<{
-    resultsToDisplay: ResultsTableRow[]
-    innerRadius: number
-    outerRadius: number
-  }>()
+interface Props {
+  resultsToDisplay: ResultsTableRow[]
+  innerRadius: number
+  outerRadius: number
+}
 
-  var innerRadius: number = props.innerRadius
-  var outerRadius: number = props.outerRadius
-  
-  var dotRadius: number
-  const desiredDots: number = 460
+const props = defineProps<Props>()
 
-  {
-    function circleArea(R: number): number {
-      return Math.PI * R * R
+const DESIRED_DOTS = 460
+
+// Interfaces
+interface Orbit {
+  radius: number
+  numberOfDots: number
+  anglePerBall: number
+}
+
+interface ParliamentCoord {
+  orbit: number
+  index: number
+}
+
+interface Dot {
+  position: ParliamentCoord
+  colorIndex: number
+}
+
+// Utility functions
+const circleArea = (radius: number): number => Math.PI * radius * radius
+
+const calculateDotsFitting = (
+  innerRadius: number,
+  outerRadius: number,
+  dotRadius: number
+): number => {
+  if (outerRadius <= innerRadius || Math.abs(outerRadius - innerRadius) < 2.0 * dotRadius) {
+    throw new Error(
+      `Invalid arguments: innerRadius(${innerRadius}), outerRadius(${outerRadius}), dotRadius(${dotRadius})`
+    )
+  }
+
+  let result = 0
+  for (let iRadius = innerRadius + dotRadius; iRadius <= outerRadius - dotRadius; iRadius += 2 * dotRadius) {
+    const radialDotLength = 2 * Math.asin(dotRadius / iRadius)
+    const numberOfDots = Math.floor(Math.PI / radialDotLength)
+    result += numberOfDots
+  }
+
+  return result
+}
+
+const findOptimalDotRadius = (
+  innerRadius: number,
+  outerRadius: number,
+  desiredDots: number
+): number => {
+  const totalArea = (circleArea(outerRadius) - circleArea(innerRadius)) / 2
+  let lowerBound = Math.sqrt(totalArea / 2 / desiredDots / 4)
+  let upperBound = Math.sqrt(totalArea * 3 / 2 / desiredDots / 4)
+
+  let dotsUpper = calculateDotsFitting(innerRadius, outerRadius, lowerBound)
+  let dotsLower = calculateDotsFitting(innerRadius, outerRadius, upperBound)
+
+  if (!(dotsLower <= desiredDots && dotsUpper >= desiredDots)) {
+    throw new Error(
+      `Desired radius out of range [${dotsLower}, ${dotsUpper}] (${desiredDots} desired)`
+    )
+  }
+
+  while (
+    !(dotsLower === desiredDots || dotsUpper === desiredDots || Math.abs(lowerBound - upperBound) < 0.000001)
+  ) {
+    const nextBound = (lowerBound + upperBound) / 2.0
+    const dotsNext = calculateDotsFitting(innerRadius, outerRadius, nextBound)
+
+    if (dotsNext <= desiredDots) {
+      upperBound = nextBound
+      dotsLower = dotsNext
+    } else {
+      lowerBound = nextBound
+      dotsUpper = dotsNext
     }
+  }
 
-    function dotsFitting(innerRadius: number, outerRadius: number, dotRadius: number) {
-      if (outerRadius <= innerRadius || Math.abs(outerRadius - innerRadius) < 2.0 * dotRadius)
-        throw new Error(`Improper arguments: innerRadius(${innerRadius}), outerRadius(${outerRadius}) and dotRadius(${dotRadius})`)
+  return dotsLower === desiredDots ? upperBound : lowerBound
+}
 
-      let result: number = 0
-      for (let iRadius = innerRadius + dotRadius; iRadius <= outerRadius - dotRadius; iRadius += 2 * dotRadius) {
-        var radialDotLength = 2 * Math.asin(dotRadius / iRadius)
-        var numberOfDots = Math.floor(Math.PI / radialDotLength)
-        result += numberOfDots
-      }
+const createOrbits = (innerRadius: number, outerRadius: number, dotRadius: number): Orbit[] => {
+  const orbits: Orbit[] = []
 
-      return result
-    }
+  for (let iRadius = innerRadius + dotRadius; iRadius <= outerRadius - dotRadius; iRadius += 2 * dotRadius) {
+    const radialDotLength = 2 * Math.asin(dotRadius / iRadius)
+    const numberOfDots = Math.floor(Math.PI / radialDotLength)
 
-    var totalArea: number = (circleArea(outerRadius) - circleArea(innerRadius)) / 2
-    var lowerBound: number = Math.sqrt(totalArea / 2 / desiredDots / 4) // Approximate number of circles that fit in the given area
-    var upperBound: number = Math.sqrt(totalArea * 3 / 2 / desiredDots / 4) // (1/2 of the total area and 3/2 of the total area)
+    orbits.push({
+      radius: iRadius,
+      numberOfDots,
+      anglePerBall: Math.PI / numberOfDots
+    })
+  }
 
-    var dotsUpper = dotsFitting(innerRadius, outerRadius, lowerBound)
-    var dotsLower = dotsFitting(innerRadius, outerRadius, upperBound)
+  return orbits
+}
 
-    if (!(dotsLower <= desiredDots && dotsUpper >= desiredDots))
-      throw new Error(`The desired radius outside of range [${dotsLower}, ${dotsUpper}] (${desiredDots} desired)`)
+const createDots = (orbits: Orbit[], resultsToDisplay: ResultsTableRow[], desiredDots: number): Dot[] => {
+  const dots: Dot[] = []
+  let currentOrbit = 0
+  let currentIndex = 0
+  let partyIndex = 0
+  let seatIndex = 0
 
-    console.log(`dotsLower = ${dotsLower}, dotsUpper = ${dotsUpper}`)
+  for (let dotIndex = 0; dotIndex < desiredDots; dotIndex++) {
+    let colorIndex = -1
     
-    while (!(dotsLower === desiredDots || dotsUpper === desiredDots || Math.abs(lowerBound- upperBound) < 0.000001)) {
-      var nextBound = (lowerBound + upperBound) / 2.0
-      var dotsNext = dotsFitting(innerRadius, outerRadius, nextBound)
-
-      if (dotsNext <= desiredDots) {
-        upperBound = nextBound
-	dotsLower = dotsNext
+    if (partyIndex < resultsToDisplay.length) {
+      colorIndex = partyIndex
+      seatIndex++
+      if (seatIndex >= resultsToDisplay[partyIndex].seats) {
+        seatIndex = 0
+        partyIndex++
       }
-      else {
-        lowerBound = nextBound
-	dotsUpper = dotsNext
+    }
+
+    dots.push({
+      position: { orbit: currentOrbit, index: currentIndex },
+      colorIndex
+    })
+
+    currentOrbit++
+    if (currentOrbit >= orbits.length) {
+      currentIndex++
+      currentOrbit = 0
+      
+      while (currentOrbit < orbits.length && orbits[currentOrbit].numberOfDots <= currentIndex) {
+        currentOrbit++
       }
-
-      console.log(`dotsLower = ${dotsLower}, dotsUpper = ${dotsUpper}, upperBound = ${upperBound}, lowerBound = ${lowerBound}`)
-    }
-
-    if (dotsLower === desiredDots)
-      dotRadius = upperBound
-    else if (dotsUpper  === desiredDots)
-      dotRadius = lowerBound
-    else
-      dotRadius = lowerBound
-  }
-
-  class Orbit {
-    radius: Number = 0
-    numberOfDots: Number = 0
-    anglePerBall: Number = 0
-  }
-
-  var orbits: Orbit[] = []
-  var total_dots = 0
-
-  {
-    for(let iRadius = innerRadius + dotRadius; iRadius <= outerRadius - dotRadius; iRadius += 2 * dotRadius) {
-      var nextOrbit: Orbit = new Orbit()
-      nextOrbit.radius = iRadius
-
-      var radialDotLength = 2 * Math.asin(dotRadius / iRadius)
-      nextOrbit.numberOfDots = Math.floor(Math.PI / radialDotLength)
-      nextOrbit.anglePerBall = Math.PI / nextOrbit.numberOfDots
-      orbits.push(nextOrbit)
-
-      total_dots += nextOrbit.numberOfDots
-    }
-  }
-
-  class ParliamentCoord {
-    orbit: Number = 0
-    index: Number = 0
-
-    next(): ParliamentCoord {
-      var lastCoord: ParliamentCoord = structuredClone(this)
-
-      this.orbit++
-      if (this.orbit >= orbits.length) {
-        this.index++
-        this.orbit = 0
-	for (const orbitIterator of orbits)
-          if (orbitIterator.numberOfDots <= this.index) {
-            this.orbit++
-	  }
+      
+      if (currentOrbit >= orbits.length) {
+        currentOrbit = 0
       }
-
-      return lastCoord
     }
   }
 
-  console.log(props.resultsToDisplay)
+  return dots
+}
 
-  class SeatIterator {
-    party: Number = 0
-    index: Number = 0
+// Computed properties
+const dotRadius = computed(() => 
+  findOptimalDotRadius(props.innerRadius, props.outerRadius, DESIRED_DOTS)
+)
 
-    next() {
-      if (this.party >= props.resultsToDisplay.length)
-        return -1
+const orbits = computed(() => 
+  createOrbits(props.innerRadius, props.outerRadius, dotRadius.value)
+)
 
-      var lastParty = this.party
-      this.index++
-      if (this.index >= props.resultsToDisplay[this.party].seats) {
-        this.index = 0
-	this.party++
-      }
-      return lastParty
-    }
+const dots = computed(() => 
+  createDots(orbits.value, props.resultsToDisplay, DESIRED_DOTS)
+)
+
+// Methods to calculate positions
+const calculateDotX = (dot: Dot): number => {
+  const orbit = orbits.value[dot.position.orbit]
+  return props.outerRadius + orbit.radius * Math.cos(orbit.anglePerBall * (dot.position.index + 0.5))
+}
+
+const calculateDotY = (dot: Dot): number => {
+  const orbit = orbits.value[dot.position.orbit]
+  return props.outerRadius - orbit.radius * Math.sin(orbit.anglePerBall * (dot.position.index + 0.5))
+}
+
+const getColorForDot = (colorIndex: number): string => {
+  if (colorIndex === -1) {
+    return 'hsl(0, 0%, 0%)'
   }
 
-  class Dot {
-    position: ParliamentCoord 
-    colorIndex: Number 
-  }
-
-  var dots: Dot[] = []
-
-  var beginning = new ParliamentCoord()
-  var seatIterator = new SeatIterator()
-  for (var dotIndex = 0; dotIndex < desiredDots; dotIndex++) {
-    var dot = new Dot()
-    dot.position = beginning.next()
-    dot.colorIndex = seatIterator.next()
-    dots.push(dot)
-  }
-
-  console.log(`total dots = ${total_dots}`)
-  console.log(orbits)
-
-  function getColorForDot(colorIndex: number): string {
-    if (colorIndex === -1) {
-      return 'hsl(0, 0%, 0%)'
-    }
-  
-    const maxColorIndex = props.resultsToDisplay.length
-    const hue = maxColorIndex > 0 ? (colorIndex / maxColorIndex) * 360 : 0
-    return `hsl(${hue}, 100%, 50%)`
-  }
-
-  console.log(dots)
+  const maxColorIndex = props.resultsToDisplay.length
+  const hue = maxColorIndex > 0 ? (colorIndex / maxColorIndex) * 360 : 0
+  return `hsl(${hue}, 100%, 50%)`
+}
 </script>
+
+<style scoped>
+.parliament-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.5rem;
+  padding: 1.5rem;
+}
+
+.legend-container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  justify-content: center;
+  max-width: 800px;
+}
+
+.legend-item {
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+}
+
+.legend-text {
+  font-size: 0.875rem;
+  font-weight: 500;
+}
+</style>
