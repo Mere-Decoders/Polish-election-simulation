55c42eba8e18b5f099909cfc9db49a3bcc32f048
StKucharski <stanislawkucharski205@gmail.com>
2025-12-03 19:18:41 +0100

    second set of election data

--- backend/backend/Services/SimData/SimDataService.cs
+++ backend/backend/Services/SimData/SimDataService.cs
@@ -1,5 +1,6 @@
 using backend.Data;
 using backend.Infrastructure.Helpers;
+using backend.Models;
 
 namespace backend.Services;
 
@@ -7,7 +8,15 @@ public class SimDataService:ISimDataService
 {
     public async Task<SimulationData> GetSimDataByGuid(Guid dataGuid)
     {
-        return SimulationDataFromCsv.Get();
+        switch (dataGuid.ToString())
+        {
+            case ("00000000-0000-0000-0000-000000000000"):
+                return SimulationDataFromCsv.Get();
+            case ("00000000-0000-0000-0000-000000000001"):
+                return SimulationDataFromCsv.Get2();
+            default:
+                throw new ArgumentException("The method you are requesting doesn't exist");
+        }
     }
 
     public async Task<List<SimulationData>> GetUsersSimData(Guid userGuid)
