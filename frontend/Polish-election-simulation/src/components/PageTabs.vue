07512eef81bc9412e37ff096115d68c0036b4e64
Piotr Janiszewski <piotr.3.janiszewski@gmail.com>
2025-12-07 19:15:21 +0100

    Pimped up the tabs selector

--- frontend/Polish-election-simulation/src/components/PageTabs.vue
+++ frontend/Polish-election-simulation/src/components/PageTabs.vue
@@ -6,7 +6,7 @@
       @click="activeTab = tab.name"
       :class="{ active: activeTab === tab.name}"
     >
-      {{ tab.title }}
+      >&nbsp{{ tab.title }}
     </button>
   </div>
 
@@ -37,7 +37,13 @@ activeTab.value = props.tabs[0].name
 <style scoped>
 .tabs {
   display: flex;
+  flex-direction: column;
+  width: max-content;
+  height: max-content;
   gap: 0.5rem;
+
+  border-left: 3px solid darkred;
+  padding-left: 10px;
 }
 .active {
   font-weight: bold;
@@ -46,5 +52,39 @@ activeTab.value = props.tabs[0].name
 .tab-content {
   margin-top: 1rem;
 }
-</style>
 
+.tabs button {
+  text-align: left;
+  padding: 5px;
+  border: 2px solid white;
+  background-color: transparent;
+  border-radius: 10px;
+  color: white;
+  transition: all 0.3s ease;
+  font-size: large;
+}
+
+.tabs button:hover {
+  background-color: darkred;
+  border-color: darkred;
+  box-shadow: 0 0 100px rgba(139, 0, 0, 1.0);
+  transform: translateX(10px);
+  cursor: pointer;
+}
+
+.tabs button:active {
+  transform: translateX(0px);
+  box-shadow: 0 0 10px rgba(255, 255, 255, 0.6);
+  background-color: white;
+  color: darkred;
+  border-color: white;
+}
+
+.tabs .active {
+  color: white;
+  background-color: darkred;
+  border-color: darkred;
+  text-decoration: none;
+}
+
+</style>
