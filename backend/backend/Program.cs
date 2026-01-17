8d5bc464b8f227fb304cbd93ba101a5a2926c177
StKucharski <stanislawkucharski205@gmail.com>
2026-01-17 18:57:58 +0100

    now its really a fix - promise

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -97,7 +97,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowLocalhost",
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173")
+            policy.AllowAnyOrigin()
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
