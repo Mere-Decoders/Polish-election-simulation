725ab42338d3a2bc72ae3af1a040536af548cf68
StKucharski <stanislawkucharski205@gmail.com>
2026-01-17 19:15:29 +0100

    as discussed altered method listings

--- backend/backend/Controllers/MethodController.cs
+++ backend/backend/Controllers/MethodController.cs
@@ -13,10 +13,13 @@ public class MethodController : ControllerBase
     [HttpGet("get-list")]
     public ActionResult< Dictionary<String, Guid>> GetMethodList()
     {
-        var options = new Dictionary<String, Guid>();
-        options.Add("Metoda Dhondt'a", Guid.Empty);
-        options.Add("Metoda High Stakes", Guid.Parse("00000000-0000-0000-0000-000000000001"));
-        options.Add("Metoda Sainte-Lague", Guid.Parse("00000000-0000-0000-0000-000000000002"));
+        var options = new[]
+        {
+            new { name = "Metoda Dhondt'a", id = Guid.Empty },
+            new { name = "Metoda High Stakes", id = Guid.Parse("00000000-0000-0000-0000-000000000001") },
+            new { name = "Metoda Sainte-Lague", id = Guid.Parse("00000000-0000-0000-0000-000000000002") }
+        };
+
         return Ok(options);
     }
     
