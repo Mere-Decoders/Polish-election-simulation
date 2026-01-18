13376651789d49d65b01ab809925ba91cc6ec5e6
d13g0h0h0 <diego.jamroz@gmail.com>
2026-01-18 13:57:47 +0100

    Remove all TypeScript errors, enable building frontend

--- frontend/Polish-election-simulation/src/api/Results.ts
+++
@@ -1,9 +0,0 @@
-import PartyConstituencyResults from './PartyConstituencyResults.ts'
-
-export default class Results {
-  public constituencies: Record<number, PartyConstituencyResults[]>;
-
-  public constructor() {
-    this.constituencies = {}
-  }
-}
