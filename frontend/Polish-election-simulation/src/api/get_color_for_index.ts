0ad818a8265e99593bc69ed552713e4d88d29b4a
Piotr janiszewski <ptaszor3@gmail.com>
2026-01-22 17:05:44 +0100

    Removed unnecesasary logs

--- frontend/Polish-election-simulation/src/api/get_color_for_index.ts
+++ frontend/Polish-election-simulation/src/api/get_color_for_index.ts
@@ -3,14 +3,9 @@ export default function get_color_for_index(colorIndex: number, maxColorIndex: n
     return 'hsl(0, 0%, 0%)'
   }
 
-  console.log(get_color_for_index);
-
-  //const hue = ((((colorIndex + 1) * get_color_for_index.interval) % get_color_for_index.upperBound) / get_color_for_index.upperBound) * 360
   let hue = 0
   for (let i = 0; i < colorIndex; i++)
   	hue = nextColor(hue)
-
-  console.log(hue)
 
   return `hsl(${hue * 360}, 100%, 50%)`
 }
