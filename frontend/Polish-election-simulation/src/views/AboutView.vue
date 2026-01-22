6654cfbb7c510a79c421ac22c1794245221f5b95
d13g0h0h0 <diego.jamroz@gmail.com>
2026-01-22 20:27:41 +0100

    Add custom tab icon & name

--- frontend/Polish-election-simulation/src/views/AboutView.vue
+++ frontend/Polish-election-simulation/src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <h1>This is the about page</h1>
+  <a href="https://www.flaticon.com/free-icons/ballot" title="ballot icons">Ballot icons created by juicy_fish - Flaticon</a>
 </template>
 
 <style>
