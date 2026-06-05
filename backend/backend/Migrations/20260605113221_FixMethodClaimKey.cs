b6a1f9a66ae368f54528dd7e63a71d36d4278b95
StarszyLicz <madmarcin33@gmail.com>
2026-06-05 13:34:32 +0200

    fixed label in method claims being key

---
+++ backend/backend/Migrations/20260605113221_FixMethodClaimKey.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixMethodClaimKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_method_claims",
+                table: "method_claims");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_method_claims",
+                table: "method_claims",
+                columns: new[] { "UserId", "MethodId" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_method_claims",
+                table: "method_claims");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_method_claims",
+                table: "method_claims",
+                columns: new[] { "UserId", "Label", "MethodId" });
+        }
+    }
+}
